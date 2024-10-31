58ee14851501cab7f95318e97046a63b090c1347
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2024-10-31 17:01:09 +0100

    solve hw5

--- task_no2.py
+++ task_no2.py
@@ -3,9 +3,12 @@ from typing import Generator, Callable
 
 def generator_numbers(text: str) -> Generator:
 
-    for word in text.split():
-        if word.strip()[0].isdigit():
-            yield float(word.strip())
+    for word in text.split(" "):
+        try:
+            if f" {word} " in text:
+                yield float(word)
+        except ValueError:
+            continue
 
 
 def sum_profit(text: str, func: Callable) -> float:
